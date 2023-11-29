0d6ea1e7fb7bf446c4bae6f2d8be9dbb4945e767
Saif Mustafa <saifsm1994@gmail.com>
2023-11-28 20:43:15 -0800

    Reverted SpreadSheet.tsx to its state before the prev commits

--- src/Components/SpreadSheet.tsx
+++ src/Components/SpreadSheet.tsx
@@ -32,15 +32,7 @@ function SpreadSheet({ documentName, spreadSheetClient }: SpreadSheetProps) {
   const [currentCell, setCurrentCell] = useState(spreadSheetClient.getWorkingCellLabel());
   const [currentlyEditing, setCurrentlyEditing] = useState(spreadSheetClient.getEditStatus());
   const [userName, setUserName] = useState(window.sessionStorage.getItem('userName') || "");
-  const [isAdmin, setIsAdmin] = useState(window.sessionStorage.getItem('isAdmin') === 'true');
-  const [isAdminKey, setIsAdminKey] = useState(window.sessionStorage.getItem('isAdminKey') || "");
-  const [isUnlocked, setIsUnlocked] = useState(
-    JSON.parse(window.sessionStorage.getItem('lockedSheets') || "[]")
-  );
-  const [userBypassPermissions, setUserBypassPermissions] = useState(() => {
-    const savedPermissions = window.sessionStorage.getItem('userBypassPermissions');
-    return savedPermissions ? JSON.parse(savedPermissions) : [];
-  });  const [serverSelected, setServerSelected] = useState("localhost");
+  const [serverSelected, setServerSelected] = useState("localhost");
 
 
   function updateDisplayValues(): void {
@@ -54,73 +46,15 @@ function SpreadSheet({ documentName, spreadSheetClient }: SpreadSheetProps) {
     setCurrentlyEditing(spreadSheetClient.getEditStatus());
   }
 
-  // useEffect to refetch the data every 1/20 of a second todo
+  // useEffect to refetch the data every 1/20 of a second
   useEffect(() => {
-    const documentName = spreadSheetClient.documentName;
-    if (isUnlocked.includes(documentName)) {
-      setIsUnlocked(true);
-    } else {
-      setIsUnlocked(false);
-    }
-
-    // Interval to fetch updated sheet data every 30 seconds todo
     const interval = setInterval(() => {
-      const sheets = spreadSheetClient.getSheets();
-      const data = dummyGetSpreadSheetData(sheets);
-
-      // Update isUnlocked state based on fetched data todo
-      setIsUnlocked(
-        sheets.filter(sheet => data[sheet].isUnlocked)
-      );
-
-      // Notify server of active user for each sheet todo
-      sheets.forEach(sheetName => {
-        // server.userIsActive({ username: userName, sheetName });
-      });
-    }, 30000);
-
+      updateDisplayValues();
+    }, 50);
     return () => clearInterval(interval);
-  }, [userName]);
-    // const interval = setInterval(() => {
-    //   updateDisplayValues();
-    // }, 50);
-    // return () => clearInterval(interval);
-  // });
-  
-  interface SheetData {
-    isUnlocked: boolean;
-    activeUsers: string[];
-  }
-interface SheetsDataType {
-  [key: string]: SheetData;
-}
-
-//  todo
-  function dummyGetSpreadSheetData(sheets: string[]): SheetsDataType {
-    const data: SheetsDataType = {};
-  
-    // Specific sheets to modify
-    const specificSheets = ['test1', 'test10.1', 'test10', 'test11', 'test12', 'test13'];
-    let index = 1;
-  
-    sheets.forEach(sheet => {
-      if (specificSheets.includes(sheet)) {
-        // For specific sheets, set isUnlocked to false and add fake usernames
-        data[sheet] = {
-          isUnlocked: false,
-          activeUsers: Array.from({ length: index++ }, (_, i) => `user${i + 1}`)
-        };
-      } else if (!data[sheet]) {
-        // For other sheets, retain existing logic
-        data[sheet] = { isUnlocked: true, activeUsers: [] };
-      }
-    });
-  
-    return data;
-  }
+  });
 
   function returnToLoginPage() {
-    //  todo - automatic call method if logged out
 
     // set the document name
     spreadSheetClient.documentName = documentName;
@@ -143,47 +77,6 @@ interface SheetsDataType {
     }
     return true;
   }
-
-  function checkPerms(): boolean {
-    //  todo
-    const documentName = spreadSheetClient.documentName;
-
-    // check if sheet is locked
-    if (isUnlocked) {
-      // unlocked return true
-      return true;
-    } else {
-      // check if user has bypass permissions
-      if (userBypassPermissions.includes(documentName)) {
-        // yes return true
-        return true;
-      } else {
-        // check if user is admin
-        if (isAdmin) {
-          return true;
-          //  yes return true
-        } else {
-          // no return false
-          // return false;
-        }
-      }
-      return true;
-    }
-  }
-
-
-  function checkPermsMessenger(): boolean {
-    //  todo - but in the sub-component ChatComponent.
-    // maybe define here and pass to chatComponent
-    // is messenger locked?
-    // if no return true
-
-    // if yes does user have by pass perrmission
-    // if yes 
-      return true;
-      // else return false
-    }
- 
 
   /**
    * 
@@ -265,10 +158,6 @@ interface SheetsDataType {
    * If the edit status is false then it will ask the machine to update the current formula.
    */
   function onCellClick(event: React.MouseEvent<HTMLButtonElement>): void {
-
-    if(!checkPerms()){
-      alert( "Sheet is locked")
-    }
 
     if (userName === "") {
       alert("Please enter a user name");
